993808dcce7c2ec3912b706d931402c418a96e27
Achille Calegari <achille.calegari@therope.it>
2016-05-18 04:10:55 +0200

    Andiamo per passi

--- templates/customer.php
+++ templates/customer.php
@@ -9,35 +9,25 @@
     <link rel="stylesheet" href="/css/common.css">
     <link rel="stylesheet" href="/css/customer.css">
 </head>
-<body style=" margin: 0; padding: 0">
-<button class="service-request-btn" data-toggle="modal" data-target="#service-request-modal" style="margin: 0; background: transparent; padding: 0; border: none;"><img src="/css/500x400.jpg" style="width: 100%; max-width: 500px;"></button>
-
-    
+<body style=" margin: 0; padding: 0">    
 
     <!-- Service Request Modal -->
-    <div class="modal fade" id="service-request-modal" tabindex="-1" role="dialog" aria-labelledby="service-request-modal-label" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-body">
-                    <form id="submittanza" role="form" class="request-form">
+    <form id="submittanza" role="form" class="request-form">
                         <div class="form-group">
                             <label for="customer-name" class="control-label">Il tuo nome:</label>
                             <input type="text" class="form-control customer-name" name="customer_name" required="required" value="Cliente">
                         </div>
                         <div class="form-group">
                             <label for="problem-text" class="control-label">Tell us about your problem:</label>
-                            <textarea class="form-control problem-text" name="problem_text" required="required" value="Problema"></textarea>
+                            <input type="text" class="form-control problem-text" name="problem_text" required="required" value="Problema">
                         </div>
 
                     </form>
-                </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-default" data-dismiss="modal">Chiudi</button>
                     <button type="button" class="btn btn-primary request-submit">Request a representative</button>
                 </div>
-            </div>
-        </div>
-    </div>
+
 
     <!-- Service Panel -->
     <div id="service-panel" class="video-holder">
