f9fe4db8504f38904e0d1d23a7ad7636c949a768
Achille Calegari <achille.calegari@therope.it>
2016-05-18 11:00:39 +0200

    Finora bene

--- templates/customer.php
+++ templates/customer.php
@@ -9,55 +9,8 @@
     <link rel="stylesheet" href="/css/customer.css">
 </head>
 <body>
-    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-      <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="#">OpenTok Service Kit</a>
-        </div>
-      </div>
-    </nav>
 
-    <!-- Main jumbotron for a primary marketing message or call to action -->
-    <div class="jumbotron">
-      <div class="container">
-        <h1>Your Amazing Product</h1>
-        <p>Here is a product that your customers need. Naturally, they may have a couple questions. Fortunately, you can use <a href="https://tokbox.com/opentok">OpenTok</a> to connect them to a representative who can answer those questions, troubleshoot, or help them making a buying decision.</p>
-        <p><button class="btn btn-primary btn-lg service-request-btn" data-toggle="modal" data-target="#service-request-modal">Talk to a Representative</button></p>
-      </div>
-    </div>
-
-    <div class="container">
-      <!-- Example row of columns -->
-      <div class="row">
-        <div class="col-md-4">
-          <h2>Feature</h2>
-          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
-        </div>
-        <div class="col-md-4">
-          <h2>Feature</h2>
-          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
-       </div>
-        <div class="col-md-4">
-          <h2>Feature</h2>
-          <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
-        </div>
-      </div>
-
-      <hr>
-
-      <footer>
-        <p>&copy; Your Company 2014</p>
-      </footer>
-    </div> <!-- /container -->
+    <button class="btn btn-primary btn-lg service-request-btn" data-toggle="modal" data-target="#service-request-modal">Start</button>
 
     <!-- Service Request Modal -->
     <div class="modal fade" id="service-request-modal" tabindex="-1" role="dialog" aria-labelledby="service-request-modal-label" aria-hidden="true">
@@ -65,23 +18,23 @@
             <div class="modal-content">
                 <div class="modal-header">
                     <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
-                    <h4 class="modal-title" id="service-request-modal-label">Talk to a Representative</h4>
+                    <h4 class="modal-title" id="service-request-modal-label">Parla con l'esperto</h4>
                 </div>
                 <div class="modal-body">
                     <form role="form" class="request-form">
                         <div class="form-group">
-                            <label for="customer-name" class="control-label">Your Name:</label>
-                            <input type="text" class="form-control customer-name" name="customer_name" required="required">
+                            <label for="customer-name" class="control-label">Nome:</label>
+                            <input type="text" class="form-control customer-name" name="customer_name" required="required" value="Pippo">
                         </div>
                         <div class="form-group">
-                            <label for="problem-text" class="control-label">Tell us about your problem:</label>
-                            <textarea class="form-control problem-text" name="problem_text" required="required"></textarea>
+                            <label for="problem-text" class="control-label">Email:</label>
+                            <input type="text" class="form-control problem-text" name="problem_text" required="required" value="pippo@pippo.it"></textarea>
                         </div>
                     </form>
                 </div>
                 <div class="modal-footer">
-                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary request-submit">Request a representative</button>
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Chiudi</button>
+                    <button type="button" class="btn btn-primary request-submit">Avvia la videochiamata</button>
                 </div>
             </div>
         </div>
@@ -96,7 +49,7 @@
         </div>
         <div class="waiting">
             <p class="hardware-access">Please allow access to your camera and mic.</p>
-            <p class="representative">Waiting for representative...</p>
+            <div class="representative"><iframe src="https://www.youtube.com/embed/bQtKOJP--uA?autoplay=1" frameborder="0" allowfullscreen></iframe></div>
         </div>
     </div>
 
