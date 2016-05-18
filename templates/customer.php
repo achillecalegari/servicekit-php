4be9c9625e5a78709685da908926f6fd85b0cfe3
Achille Calegari <achille.calegari@therope.it>
2016-05-18 10:07:27 +0200

    Date!!

--- templates/customer.php
+++ templates/customer.php
@@ -15,37 +15,47 @@
     
 
     <!-- Service Request Modal -->
-    <div class="modal fade" id="service-request-modal" tabindex="-1" role="dialog" aria-labelledby="service-request-modal-label" aria-hidden="true">
+    <div class="modal" id="service-request-modal" tabindex="-1" role="dialog" aria-labelledby="service-request-modal-label" aria-hidden="true">
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-body">
                     <form role="form" class="request-form">
                         <div class="form-group">
                             <label for="customer-name" class="control-label">Il tuo nome:</label>
-                            <input type="text" class="form-control customer-name" name="customer_name" required="required" value="Cliente">
+                            <input type="text" class="form-control customer-name" name="customer_name" required="required">
+                        </div>
+                        <div class="form-group">
+                            <label for="problem-text" class="control-label">La tua email:</label>
+                            <textarea class="form-control problem-text" name="problem_text" required="required"></textarea>
                         </div>
 
                     </form>
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-default" data-dismiss="modal">Chiudi</button>
-                    <button type="button" class="btn btn-primary request-submit">Request a representative</button>
+                    <button type="button" class="btn btn-primary request-submit">Parla con un nostro consulente</button>
                 </div>
             </div>
         </div>
     </div>
 
-    <!-- Service Panel -->
-    <div id="service-panel" class="video-holder">
-        <div class="subscriber"></div>
-        <div class="publisher"></div>
-        <div class="bottom-bar">
-            <button type="button" class="btn btn-danger close-button">Cancel</button>
-        </div>
-        <div class="waiting">
-            <iframe src="https://www.youtube.com/embed/bQtKOJP--uA?autoplay=1" frameborder="0" allowfullscreen></iframe>
-        </div>
+    <div class="container waiting">
+            <header>
+                <img src="images/logo.jpg"\>
+                <h1 class="onhold" id="status">Videochiamata in corso</h1>
+            </header>
+            <section id="service-panel" class="video-holder">
+                <iframe class="waiting" width="100%" height="500" src="https://www.youtube.com/embed/bbfdX6SQYn4?autoplay=1" frameborder="0" allowfullscreen></iframe>
+                 <div class="subscriber"></div>
+                <div class="publisher"></div>
+            </section>
+            <footer>
+                <a id="microfono"><img src="images/microfono.png"></a>
+                <a id="chat"><img src="images/chat.png"></a>
+                <a id="interrompi close-button"><img src="images/interrompi.png"></a>
+            </footer>
     </div>
+
 
     <script src="//cdnjs.cloudflare.com/ajax/libs/lodash.js/2.4.1/lodash.min.js"></script>
     <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
